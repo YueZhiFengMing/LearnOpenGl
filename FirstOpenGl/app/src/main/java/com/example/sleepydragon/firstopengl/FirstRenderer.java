dc465164f7cf39969f5db167c6b0d37be189f703
sleepydragon <xlspace@sina.com>
2018-10-21 22:30:18 +0800

    First demo like hello world!

---
+++ FirstOpenGl/app/src/main/java/com/example/sleepydragon/firstopengl/FirstRenderer.java
@@ -0,0 +1,33 @@
+package com.example.sleepydragon.firstopengl;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
+public class FirstRenderer implements GLSurfaceView.Renderer {
+
+    private static final String TAG = "FirstRenderer";
+
+    @Override
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 glUnused) {
+
+    }
+
+}
--- FirstOpenGl/app/src/main/java/com/example/sleepydragon/firstopengl/MainActivity.java
+++ FirstOpenGl/app/src/main/java/com/example/sleepydragon/firstopengl/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.sleepydragon.firstopengl;
 
-import android.support.v7.app.AppCompatActivity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    GLSurfaceView mGlSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGlSurfaceView = findViewById(R.id.surfaceview);
+        // Request an OpenGL ES 2.0 compatible context.
+        mGlSurfaceView.setEGLContextClientVersion(2);
+        mGlSurfaceView.setRenderer(new FirstRenderer());
     }
 }
